dc0362f371622870d901f75d395aff50aace949d
limserhane <limserhane@protonmail.com>
2021-08-17 14:03:48 +0200

    strings

--- goprint.go
+++ goprint.go
@@ -2,26 +2,27 @@ package goprint
 
 import "fmt"
 
-func Red(tag string, description string) {
-	fmt.Printf("\033[31m[%s] %s\033[0m", tag, description)
+func Red(format string, args ...interface{}) (string) {
+	// fmt.Printf("\033[31m[%s] %s\033[0m", tag, description)
+	return fmt.Sprintf("\033[31m" + format + "\033[0m", args...)
 }
 
-func Yellow(tag string, description string) {
-	fmt.Printf("\033[33m[%s] %s\033[0m", tag, description)
+func Yellow(format string, args ...interface{}) (string) {
+	return fmt.Sprintf("\033[33m" + format + "\033[0m", args...)
 }
 
-func White(tag string, description string) {
-	fmt.Printf("\033[37m[%s] %s\033[0m", tag, description)
+func White(format string, args ...interface{}) (string) {
+	return fmt.Sprintf("\033[37m" + format + "\033[0m", args...)
 }
 
-func Green(tag string, description string) {
-	fmt.Printf("\033[32m[%s] %s\033[0m", tag, description)
+func Green(format string, args ...interface{}) (string) {
+	return fmt.Sprintf("\033[32m" + format + "\033[0m", args...)
 }
 
-func Magenta(tag string, description string) {
-	fmt.Printf("\033[35m[%s] %s\033[0m", tag, description)
+func Magenta(format string, args ...interface{}) (string) {
+	return fmt.Sprintf("\033[35m" + format + "\033[0m", args...)
 }
 
-func Blue(tag string, description string) {
-	fmt.Printf("\033[34m[%s] %s\033[0m", tag, description)
+func Blue(format string, args ...interface{}) (string) {
+	return fmt.Sprintf("\033[34m" + format + "\033[0m", args...)
 }
